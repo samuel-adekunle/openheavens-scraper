feae68e08a226ea107c9e1d9593c0ddd2f5b48fe
Samuel Adekunle <me@samuel-adekunle.software>
2023-07-19 14:29:48 +0100

    specify port via flags

---
+++ server/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	port := flag.Int("port", 8080, "port to serve")
+	http.Handle("/", http.FileServer(http.Dir("../")))
+	fmt.Printf("Server is running at port %d\n", *port)
+	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+}
