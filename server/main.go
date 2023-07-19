e47c46560e9890a16cd44627148641a19f973cf6
Samuel Adekunle <me@samuel-adekunle.software>
2023-07-19 15:26:18 +0100

    forgot to parse flags

--- server/main.go
+++ server/main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	port := flag.Int("port", 8080, "port to serve")
+	flag.Parse()
 	http.Handle("/", http.FileServer(http.Dir("../")))
 	fmt.Printf("Server is running at port %d\n", *port)
 	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
