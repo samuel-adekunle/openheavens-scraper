de3eb35c6329e6a256dce8ad4fd8b8e7e72924d8
Samuel Adekunle <me@samuel-adekunle.software>
2023-07-20 07:26:15 +0100

    deleting test comment

--- server/main.go
+++ server/main.go
@@ -7,7 +7,6 @@ import (
 )
 
 func main() {
-	// TODO: delete this comment, testing auto deploy
 	port := flag.Int("port", 8080, "port to serve")
 	flag.Parse()
 	http.Handle("/", http.FileServer(http.Dir("../")))
