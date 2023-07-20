7f957d96529bf9475b55e945529902fb0e7d6ffd
Samuel Adekunle <me@samuel-adekunle.software>
2023-07-20 16:35:45 +0100

    add health endpoint to server

--- server/main.go
+++ server/main.go
@@ -4,12 +4,36 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+
+	"github.com/heptiolabs/healthcheck"
 )
 
-func main() {
-	port := flag.Int("port", 8080, "port to serve")
-	flag.Parse()
+var PORT int
+
+func init() {
+	PORT = *flag.Int("port", 8080, "port to serve")
+}
+
+func addHealthChecks(health healthcheck.Handler) {
+	// TODO(samuel-adekunle): add liveness and readiness checks
+	// TODO(samuel-adekunle): add prometheus metrics
+}
+
+func addHealthEndpoints(health healthcheck.Handler) {
+	http.Handle("/healthz", http.HandlerFunc(health.LiveEndpoint))
+	http.Handle("/statusz", http.HandlerFunc(health.ReadyEndpoint))
+}
+
+func addEndpoints(health healthcheck.Handler) {
+	addHealthEndpoints(health)
 	http.Handle("/", http.FileServer(http.Dir("../")))
-	fmt.Printf("Server is running at port %d\n", *port)
-	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+}
+
+func main() {
+	flag.Parse()
+	health := healthcheck.NewHandler()
+	addHealthChecks(health)
+	addEndpoints(health)
+	fmt.Printf("Server is running at port %d\n", PORT)
+	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", PORT), nil)
 }
