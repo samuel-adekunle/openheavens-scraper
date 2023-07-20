a87732025a7475880ccc8ee1ae0078af2aa08588
Samuel Adekunle <me@samuel-adekunle.software>
2023-07-20 16:38:54 +0100

    fix port issues

--- server/main.go
+++ server/main.go
@@ -8,12 +8,6 @@ import (
 	"github.com/heptiolabs/healthcheck"
 )
 
-var PORT int
-
-func init() {
-	PORT = *flag.Int("port", 8080, "port to serve")
-}
-
 func addHealthChecks(health healthcheck.Handler) {
 	// TODO(samuel-adekunle): add liveness and readiness checks
 	// TODO(samuel-adekunle): add prometheus metrics
@@ -30,10 +24,11 @@ func addEndpoints(health healthcheck.Handler) {
 }
 
 func main() {
+	port := flag.Int("port", 8080, "port to serve")
 	flag.Parse()
 	health := healthcheck.NewHandler()
 	addHealthChecks(health)
 	addEndpoints(health)
-	fmt.Printf("Server is running at port %d\n", PORT)
-	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", PORT), nil)
+	fmt.Printf("Server is running at port %d\n", *port)
+	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", *port), nil)
 }
