e1c6068fbec8bbe4d5e055eae0ddc33ddd206b0a
Samuel Adekunle <me@samuel-adekunle.software>
2023-07-22 13:20:15 +0100

    only run later steps if there is a change detected

---
+++ scripts/send-notification.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+TODAY=$(date +"%-d-%B-%Y" | tr "[:upper:]" "[:lower:]")
+go run ../twilio/main.go -date=${TODAY}
--- twilio/send-notification.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-TODAY=$(date +'%-d-%B-%Y' | tr '[:upper:]' '[:lower:]')
-go run main.go -date=${TODAY}
