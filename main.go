d10066aabfe842b5788bfae15aedf779d9358c96
Samuel Adekunle <me@samuel-adekunle.software>
2023-07-22 08:50:16 +0100

    change scraper to flattimes

--- main.go
+++ main.go
@@ -3,109 +3,82 @@ package main
 import (
 	"log"
 	"os"
-	"regexp"
 	"strings"
 	"time"
 
 	"github.com/gocolly/colly"
 )
 
-var DATE_FORMATS = []string{
-	"2-January-2006-Monday",
-	"2-January-2006",
-}
+const (
+	DATE_FORMAT = "02-January-2006"
+	USER_AGENT  = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+	BASE_URL    = "https://flatimes.com/open-heaven-"
+)
 
-const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
-const BASE_URL = "https://rccgonline.org/open-heaven-"
+func sanitizeString(s string) string {
+	return strings.TrimSpace(s)
+}
 
 func parsePostHTML(e *colly.HTMLElement) (post *Post) {
 	post = new(Post)
-	post.Title = e.DOM.Find("div.entry-content > h1").Clone().Children().Remove().End().Text()
-	removeMediaRegexp := regexp.MustCompile(`@media.*;`)
-	// mode 0 - memory verse
-	// mode 1 - bible reading heading
-	// mode 2 - bible reading body
-	// mode 3 - wait for empty line
-	// mode 4 - message body & skip action point header
-	// mode 5 - action point body
-	// mode 6 - wait for empty line
-	// mode 7 - skip hymn header
-	// mode 8 - hymn title & (maybe) bible in one year
-	// mode 9 - (if hymn) hymn body & bible in one year
-	// mode 10 - post.Authors & finish
-	mode := 0
-	e.ForEach("div.entry-content > p", func(_ int, el *colly.HTMLElement) {
-		switch mode {
+	state := 0
+
+	e.ForEach(".et_pb_text_inner h2, .et_pb_text_inner p:not(.has-text-align-center)", func(_ int, el *colly.HTMLElement) {
+		switch state {
 		case 0:
-			if strings.Contains(el.Text, "MEMORIZE") {
-				post.MemoryVerse = strings.TrimSpace(el.DOM.Find("em, span").Clone().Children().Remove().End().Text())
-				post.MemoryVerse = post.MemoryVerse + " " + strings.TrimSpace(el.DOM.Find("strong:nth-child(3)").Clone().Children().Remove().End().Text())
-				mode = 1
-				return
+			if el.Name == "h2" {
+				state += 1
+			} else {
+				state = -1
 			}
 		case 1:
-			if strings.Contains(el.Text, "BIBLE READING") {
-				post.BibleReadingHeading = strings.TrimSpace(el.DOM.Find("strong").Clone().Children().Remove().End().Text())
-				mode = 2
-			}
+			post.Title = sanitizeString(el.Text)
+			state += 1
 		case 2:
-			if el.Text == string([]byte{194, 160}) {
-				mode = 3
-			}
+			post.MemoryVerse = sanitizeString(el.DOM.Clone().Children().Remove().End().Text())
+			state += 1
 		case 3:
-			if el.Text == string([]byte{194, 160}) {
-				mode = 4
-			}
+			post.BibleReadingHeading = sanitizeString(el.Text)
+			state += 1
 		case 4:
-			if strings.Contains(el.Text, "POINT") {
-				mode = 5
+			if strings.Contains(el.Text, "YEAR") {
+				post.BibleInOneYear = sanitizeString(el.Text)
+				state += 1
 			} else {
-				tmp := strings.TrimSpace(el.DOM.Find("span").Clone().Children().Remove().End().Text())
-				if len(tmp) > 0 {
-					post.MessageBody = append(post.MessageBody, strings.TrimSpace(
-						removeMediaRegexp.ReplaceAllString(el.ChildText("span"), "")),
-					)
-				}
+				post.BibleReadingBody = append(post.BibleReadingBody, sanitizeString(el.Text))
 			}
 		case 5:
-			post.ActionPoint = strings.TrimSpace(el.DOM.Find("span").Clone().Children().Remove().End().Text())
-			mode = 6
+			if el.Name == "h2" {
+				state += 1
+			} else {
+				state = -1
+			}
 		case 6:
-			if el.Text == string([]byte{194, 160}) {
-				mode = 7
-			} else if strings.Contains(el.Text, "HYMN") {
-				mode = 8
+			if el.Name == "h2" {
+				state += 1
+			} else {
+				post.MessageBody = append(post.MessageBody, sanitizeString(el.Text))
 			}
 		case 7:
-			if strings.Contains(el.Text, "HYMN") {
-				mode = 8
-			}
+			post.PrayerPoint = sanitizeString(el.Text)
+			state += 1
 		case 8:
-			if strings.Contains(el.Text, "ONE YEAR") {
-				post.BibleInOneYear = strings.TrimSpace(el.DOM.Find("strong").Clone().Children().Remove().End().Text())
-				mode = 10
+			if el.Name == "h2" {
+				post.HymnTitle = sanitizeString(el.Text)
+				state += 1
 			} else {
-				post.HymnTitle = strings.TrimSpace(el.DOM.Find("strong").Clone().Children().Remove().End().Text())
-				mode = 9
+				state = -1
 			}
 		case 9:
-			if strings.Contains(el.Text, "ONE YEAR") {
-				post.BibleInOneYear = strings.TrimSpace(el.DOM.Find("strong").Clone().Children().Remove().End().Text())
-				post.HymnBody = post.HymnBody[:len(post.HymnBody)-1]
-				mode = 10
+			tmp := sanitizeString(el.Text)
+			if len(tmp) > 0 && tmp[0] >= '0' && tmp[0] <= '9' {
+				post.HymnBody = append(post.HymnBody, tmp)
 			} else {
-				el.ForEach("strong", func(_ int, ell *colly.HTMLElement) {
-					post.HymnBody = append(post.HymnBody, strings.TrimSpace(ell.Text))
-				})
-				post.HymnBody = append(post.HymnBody, " ")
+				return
 			}
-		case 10:
-			el.ForEach("strong", func(_ int, ell *colly.HTMLElement) {
-				post.Authors = append(post.Authors, strings.TrimSpace(strings.Trim(ell.Text, "AUTHORS: ")))
-			})
-			mode = 11
 		default:
-			log.Println("Something went wrong: mode = ", mode)
+			log.Println("Invalid post!")
+			return
 		}
 	})
 	return post
@@ -128,7 +101,7 @@ func scrapePost(date string) *Post {
 	c.OnScraped(func(r *colly.Response) {
 		log.Println(r.Request.URL, " scraped!")
 	})
-	c.OnHTML("article", func(e *colly.HTMLElement) {
+	c.OnHTML(".et_pb_text_inner", func(e *colly.HTMLElement) {
 		if !strings.Contains(e.Request.URL.String(), date) {
 			return
 		}
@@ -140,14 +113,9 @@ func scrapePost(date string) *Post {
 }
 
 func scrapeToday() (post *Post, date string) {
-	for _, format := range DATE_FORMATS {
-		date = strings.ToLower(time.Now().Format(format))
-		post = scrapePost(date)
-		if post != nil {
-			return post, date
-		}
-	}
-	return nil, ""
+	date = strings.ToLower(time.Now().Format(DATE_FORMAT))
+	post = scrapePost(date)
+	return post, date
 }
 
 func savePost(post *Post, date string) {
--- post.go
+++ post.go
@@ -11,14 +11,13 @@ type Post struct {
 	BibleReadingHeading string   `json:"bibleReadingHeading"`
 	BibleReadingBody    []string `json:"bibleReadingBody"`
 	MessageBody         []string `json:"messageBody"`
-	ActionPoint         string   `json:"actionPoint"`
+	PrayerPoint         string   `json:"prayerPoint"`
 	HymnTitle           string   `json:"hymnTitle"`
 	HymnBody            []string `json:"hymnBody"`
 	BibleInOneYear      string   `json:"bibleInOneYear"`
-	Authors             []string `json:"authors"`
 }
 
 // string representation of a post
 func (p Post) String() string {
-	return fmt.Sprintf("*%s*\n\n*MEMORIZE:*\n%s\n\n*%s*\n%s\n\n*MESSAGE:*\n%s\n\n*ACTION POINT:*\n%s\n\n*HYMN:*\n%s\n%s\n\n*%s*\n\n*AUTHORS:*\n%s", p.Title, p.MemoryVerse, p.BibleReadingHeading, strings.Join(p.BibleReadingBody, "\n\n"), strings.Join(p.MessageBody, "\n\n"), p.ActionPoint, p.HymnTitle, strings.Join(p.HymnBody, "\n"), p.BibleInOneYear, strings.Join(p.Authors, "\n"))
+	return fmt.Sprintf("*%s*\n\n*MEMORISE:*\n%s\n\n*%s*\n%s\n\n*%s*\n\n*MESSAGE:*\n%s\n\n*PRAYER POINT:*\n%s\n\n*%s*\n%s\n", p.Title, p.MemoryVerse, p.BibleReadingHeading, strings.Join(p.BibleReadingBody, "\n\n"), p.BibleInOneYear, strings.Join(p.MessageBody, "\n\n"), p.PrayerPoint, p.HymnTitle, strings.Join(p.HymnBody, "\n\n"))
 }
