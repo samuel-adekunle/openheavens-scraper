f65a389655d43d42bb124fdf35b94c3749665d05
Samuel Adekunle <me@samuel-adekunle.software>
2023-07-22 09:13:58 +0100

    remove hardcoded date

--- scraper.sh
+++ scraper.sh
@@ -4,7 +4,6 @@ go version
 echo $(date)
 TODAY=$(date +'%-d-%B-%Y' | tr '[:upper:]' '[:lower:]')
 echo "Today is ${TODAY}"
-TODAY="21-july-2023"
 if compgen -G "./posts/${TODAY}*.txt" >/dev/null; then
     echo "Already scraped for today"
 else
