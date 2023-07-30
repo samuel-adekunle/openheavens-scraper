8073b1fa0d65294f96a9d56aedb78ac07b896644
Samuel Adekunle <me@samuel-adekunle.software>
2023-07-30 12:21:28 +0000

    remove twilio to use gmail notification for free

--- scripts/scrape-post.sh
+++ scripts/scrape-post.sh
@@ -5,7 +5,7 @@ if compgen -G "posts/${TODAY}*.txt" >/dev/null; then
     echo "Already scraped for today, ${TODAY}"
 else
     echo "Scraping for today, ${TODAY}"
-    go run main.go post.go twilio.go -date="${TODAY}" -text="posts/${TODAY}.txt" -html="posts/${TODAY}.html"
+    go run main.go post.go -date="${TODAY}" -text="posts/${TODAY}.txt" -html="posts/${TODAY}.html"
     touch .deploy
     echo "Scraped for today, ${TODAY}"
 fi
